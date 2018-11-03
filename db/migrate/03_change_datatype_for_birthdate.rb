49130afbaa1dafe66a07f9bf095372312a7978b5
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-11-03 01:35:13 +0000

    automatically backed up by learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,4 +1,4 @@
-class ChangeBirthdateDatatype < ActiveRecord::Migration[5.1]
+class ChangeDatatypeForBirthdates < ActiveRecord::Migration[5.1]
   def change
     change_column(:students, :birthdate, :datetime)
   end
